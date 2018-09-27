fe9af7b5083e68622624aec11f3eb018e2009a54
Karenqzs <43148343+Karenqzs@users.noreply.github.com>
2018-09-26 22:14:18 -0700

    Add files via upload

---
+++ 40-drops-dripping.js
@@ -0,0 +1,28 @@
+
+var x = 230;
+var y = [220, 280, 320,200,210,300,205,310,330,340,250,190,180,170,160];
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  noStroke();
+
+  // draw pipe
+  rect(0, 200, x, 20);
+
+  for (var i = 0; i < y.length; i = i + 1) {
+    // draw drip
+    ellipse(x, y[i], 10);
+
+    // down 3 pixels each frame
+    y[i] = y[i] + 3;
+
+    // if invisible for a full “height” amount, reset
+    if (y[i] > height*2) {
+      y[i] = 220;
+    }
+  }
+}
